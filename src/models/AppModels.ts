6fb959a76fef6cde1ce557d74e2e0a98c3ea50d4
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-08-04 10:21:44 -0400

    Add Application Model file

---
+++ src/models/AppModels.ts
@@ -0,0 +1,18 @@
+/**
+ * Vuex State Model
+ */
+export interface RootState {
+
+  // Flag to show if the data has been loaded
+  dataLoaded: Boolean;
+  dataError: Boolean;
+  // data: Array<TYPE>;
+
+}
+
+/**
+ * Thing Model
+ */
+// export interface Thing {
+//   id: string;
+// }
