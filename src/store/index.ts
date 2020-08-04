19492be7a4c35b189c18fad3d6996f868812d8a4
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-08-04 10:31:08 -0400

    Add boilerplate code to the vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,34 @@
 import { createStore, createLogger } from 'vuex';
+import { RootState } from '@/models/AppModels';
 
 const debug = process.env.NODE_ENV !== 'production';
 
 export default createStore({
   state: {
+    dataLoaded: false,
+    dataError: false,
+    // data: [],
+  },
+  getters: {
+    // GetAllThings: (state:RootState) => () => state.data,
   },
   mutations: {
+
+    // SET_THINGS(state: RootState, things: Thing[]) {
+    //   state.data = things;
+    //   // Set the store status fields now that the things are loaded
+    //   state.dataLoaded = true;
+    //   state.dataError = false;
+    // },
+
   },
   actions: {
+
+    // Load things from a Service
+    loadAllThings({commit}) {
+      // GenericService.loadAllThings(() => {},() => {});
+    }
+
   },
   modules: {
   },
