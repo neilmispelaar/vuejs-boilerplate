fd7d21689e131404603578d0834cd49d2d290f56
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-09-22 15:52:47 -0400

    Conver tabs to spaces

--- src/components/TheSkipNav.vue
+++ src/components/TheSkipNav.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <!-- Site Accessibility Navigation -->
-		<ul class="sr-only">
-			<li>
-				<a href="#maincontent">Skip to main content</a>
-			</li>
-		</ul>
+    <ul class="sr-only">
+      <li>
+        <a href="#maincontent">Skip to main content</a>
+      </li>
+    </ul>
   </div>
 </template>
 
